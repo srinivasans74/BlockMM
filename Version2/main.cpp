845d97afc5dc108602fb1365b6ef4f50a0b662f0
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2020-11-07 14:41:11 +0530

    Update main.cpp

--- Version2/main.cpp
+++ Version2/main.cpp
@@ -6,48 +6,19 @@ typedef ap_uint<streamlength> streamsize;
 void csrmul(hls::stream<ap_uint<streamlength>>&ina,hls::stream<ap_uint<streamlength>>&matrixbcsc,
 		hls::stream<ap_uint<streamlength>>&out)
 {
+#pragma HLS INTERFACE axis port=ina register_mode=both register
+#pragma HLS INTERFACE axis port=matrixbcsc register_mode=both register
+#pragma HLS INTERFACE axis port=out register_mode=both register
 
-	streamsize Output[size][size];
-	csrmul_label1:for (int i=0;i<size;i++)
+
+
+	csrmul_label3:for (int i=0;i<dim*size;i++)
 	{
-	#pragma HLS UNROLL
-		csrmul_label2:for (int j=0;j<size;j++)
-		{
-	#pragma HLS UNROLL
-			Output[i][j]=0;
-		}
+#pragma HLS PIPELINE
+			out.write((ina.read())*(matrixbcsc.read()));
+				}
+
+
 	}
 
-	csrmul_label3:for (int i=0;i<size;i++)
-	{
-		csrmul_label4:for (int j=0;j<2;j++)
-		{
-	#pragma HLS UNROLL factor=4
-
-			for (int k=0;k<size;k++)
-			{
-	#pragma HLS UNROLL factor=4
-	#pragma HLS PIPELINE off
-
-				Output[i][k]+=(ina.read())*(matrixbcsc.read());
-			}
-		}
-	}
-
-
-
-
-	csrmul_label5:for (int i=0;i<size;i++)
-	{
-
-	for (int j=0;j<size;j++)
-	{
-	#pragma HLS PIPELINE off
-
-	#pragma HLS UNROLL factor=4
-	out.write(Output[i][j]);
-	}
-
-	}
-}
 

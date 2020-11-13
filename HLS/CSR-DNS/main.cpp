5272012bbc63c02a3bc50313c032448596d17853
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2020-11-13 10:26:37 +0530

    Create main.cpp

---
+++ HLS/CSR-DNS/main.cpp
@@ -0,0 +1,21 @@
+#include <ap_int.h>
+#include <hls_stream.h>
+#include"header.h"
+
+typedef ap_uint<streamlength> streamsize;
+void csrmul(hls::stream<ap_uint<streamlength>>&ina,hls::stream<ap_uint<streamlength>>&matrixbcsc,
+		hls::stream<ap_uint<streamlength>>&out)
+{
+#pragma HLS INTERFACE axis port=ina register_mode=both register
+#pragma HLS INTERFACE axis port=matrixbcsc register_mode=both register
+#pragma HLS INTERFACE axis port=out register_mode=both register
+
+
+	csrmul_label3:for (int i=0;i<dim*size;i++)
+	{
+#pragma HLS PIPELINE
+			out.write((ina.read())*(matrixbcsc.read()));
+				}
+
+
+	}
